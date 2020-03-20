20a06d7f9f46aea92d6a133f343600a02cae7f62
Juan García Garland <jpgarcia@fing.edu.uy>
2020-03-20 02:23:43 -0300

    test module for syntax, it will be refactored later

---
+++ src/Language/MFAG/Syntax/Exp/Test.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Language.MFAG.Syntax.Exp.Test where
+
+import Language.Grammars.AspectAG
+import Language.Grammars.AspectAG.TH
+
+import Language.MFAG.Syntax.Terminals
+import Language.MFAG.Syntax.Set.Base     as Set
+
+import Language.MFAG.Syntax.Exp.Core     as C
+import Language.MFAG.Syntax.Exp.Unfolded as U
+
+
+--- if this compiles, dependencies are ok
