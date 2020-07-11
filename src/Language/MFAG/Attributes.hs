6e99ccb7be15225897ba627aa61938a4be024405
jpgarcia <jpgarcia@fing.edu.uy>
2020-07-11 18:02:58 -0300

    attribute definition module

---
+++ src/Language/MFAG/Attributes.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE RankNTypes #-}
+
+
+module Language.MFAG.Attributes where
+
+import Language.Grammars.AspectAG
+import Language.Grammars.AspectAG.TH
+
+import Language.MFAG.AST
+import Language.MFAG.Syntax.Terminals
+import Language.MFAG.SemFuncs
+import Language.MFAG.Utils
+import Language.MFAG.Delta
+
+import Control.Applicative
+import Prelude hiding (exp)
+import qualified Data.Map as M
+
+import Control.Monad
+
+-- Γ attribute, i.e. vatiable environment
+
+$(attLabels [("eval" , ''Val)])
