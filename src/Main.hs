c65ddfa426f50b23c1fc54c5e16fcfea43262dd4
jpgarcia <jpgarcia@fing.edu.uy>
2020-07-15 23:57:58 -0300

    update main (only deps)

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,6 @@
 module Main where
 
-import Language.MFAG.Syntax.Terminals
-import Language.MFAG.Syntax.Set.Base
-import Language.MFAG.Syntax.Exp.Core
-import Language.MFAG.Syntax.Exp.Base
-import Language.MFAG.Semantics.Env
-import Language.MFAG.Semantics.Unfold
-import Language.MFAG.Semantics.Test
+import Language.MFAG.Eval
 
 
 main :: IO ()
